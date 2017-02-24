5ecf5ed553d370e940806f9ad235737b3643503c
Habib Alamin <ha.alamin@gmail.com>
2017-02-24 23:08:19 +0000

    Set up asset pipeline.

--- src/lib/Arksink/Server.hs
+++ src/lib/Arksink/Server.hs
@@ -1,17 +1,31 @@
 module Arksink.Server (run) where
 
-import Web.Scotty (scotty)
 import qualified Configuration.Dotenv as Dotenv
 import System.Posix.Env (setEnv)
+import Web.Scotty (scotty, middleware)
+import Network.Wai.Middleware.Static (initCaching,
+                                      CachingStrategy(..),
+                                      CacheContainer(..),
+                                      staticPolicy',
+                                      addBase)
+import Network.Wai (Middleware)
 
 import qualified Landing as Landing
 
 run :: Int -> IO ()
 run port = do
     loadEnvFile
-    scotty port $ do
-        Landing.routes
+    server port =<< initCaching PublicStaticCaching
 
 loadEnvFile = Dotenv.parseFile ".env" >>= mapM_ setEnvTuple
     where
         setEnvTuple = ($ True) . flip . uncurry $ setEnv
+
+server :: Int -> CacheContainer -> IO ()
+server port cacheContainer = scotty port $ do
+    middleware $ static cacheContainer
+
+    Landing.routes
+
+static :: CacheContainer -> Middleware
+static cacheContainer = staticPolicy' cacheContainer $ addBase "public"
