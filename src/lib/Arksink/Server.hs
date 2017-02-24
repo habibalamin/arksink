0b28783f79561f7d505bb27eeec880f9eb7978f9
Habib Alamin <ha.alamin@gmail.com>
2017-02-24 22:45:54 +0000

    Load environment before starting app.

--- src/lib/Arksink/Server.hs
+++ src/lib/Arksink/Server.hs
@@ -1,9 +1,17 @@
 module Arksink.Server (run) where
 
 import Web.Scotty (scotty)
+import qualified Configuration.Dotenv as Dotenv
+import System.Posix.Env (setEnv)
 
 import qualified Landing as Landing
 
 run :: Int -> IO ()
-run port = scotty port $ do
-    Landing.routes
+run port = do
+    loadEnvFile
+    scotty port $ do
+        Landing.routes
+
+loadEnvFile = Dotenv.parseFile ".env" >>= mapM_ setEnvTuple
+    where
+        setEnvTuple = ($ True) . flip . uncurry $ setEnv
