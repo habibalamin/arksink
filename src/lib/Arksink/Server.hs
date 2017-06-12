87f276dcc30b11b86b9dcdd29d91e74b0a233116
Habib Alamin <ha.alamin@gmail.com>
2017-06-12 06:54:44 +0100

    Add middleware to log requests.

--- src/lib/Arksink/Server.hs
+++ src/lib/Arksink/Server.hs
@@ -1,8 +1,9 @@
 module Arksink.Server (run) where
 
 import qualified Configuration.Dotenv as Dotenv
-import System.Posix.Env (setEnv)
+import System.Posix.Env (setEnv, getEnvDefault)
 import Web.Scotty (scotty, middleware, notFound)
+import Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
 import Network.Wai.Middleware.Static (initCaching,
                                       CachingStrategy(..),
                                       CacheContainer(..),
@@ -17,17 +18,35 @@ import qualified Session as Session
 import qualified Bookmark as Bookmark
 import qualified General.View as General.View
 
+data Environment
+  = Development
+  | Test
+  | Production
+  | Staging
+  deriving (Show, Eq)
+
 run :: Int -> IO ()
 run port = do
   loadEnvFile
-  server port =<< initCaching PublicStaticCaching
+  environment <- environmentFromString
+    <$> getEnvDefault "ARKSINK_ENV" "development"
+  cacheContainer <- initCaching PublicStaticCaching
+  server port environment cacheContainer
+
+environmentFromString "production" = Production
+environmentFromString "staging" = Staging
+environmentFromString "test" = Test
+environmentFromString _ = Development
 
 loadEnvFile = Dotenv.parseFile ".env" >>= mapM_ setEnvTuple
   where
     setEnvTuple = ($ True) . flip . uncurry $ setEnv
 
-server :: Int -> CacheContainer -> IO ()
-server port cacheContainer = scotty port $ do
+server :: Int -> Environment -> CacheContainer -> IO ()
+server port environment cacheContainer = scotty port $ do
+  if environment == Development || environment == Test
+  then middleware logStdoutDev
+  else middleware logStdout
   middleware $ static cacheContainer
   middleware secureCookies
 
