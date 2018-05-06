9d1354873a185fd374edae4cf4891624a360cf3e
Habib Alamin <ha.alamin@gmail.com>
2018-05-06 21:15:44 +0100

    Use ARKSINK_DB_HOST environment variable to connect to PostgreSQL.
    Also, set defaults for ARKSINK_DB_NAME and ARKSINK_DB_USERNAME, as
    `"arksink"` and `"postgres"`, respectively.

--- src/lib/Database/PostgreSQL.hs
+++ src/lib/Database/PostgreSQL.hs
@@ -1,14 +1,18 @@
 module Database.PostgreSQL (connection, withConnection) where
 
 import System.Environment (getEnv)
+import System.Posix.Env (getEnvDefault)
 import Database.PostgreSQL.Simple (connect, defaultConnectInfo, Connection(..), ConnectInfo(..))
 
 connection :: IO Connection
 connection = do
-  username <- getEnv "ARKSINK_DB_USERNAME"
+  host     <- getEnvDefault "ARKSINK_DB_HOST" "localhost"
+  username <- getEnvDefault "ARKSINK_DB_USERNAME" "postgres"
   password <- getEnv "ARKSINK_DB_PASSWORD"
-  database <- getEnv "ARKSINK_DB_NAME"
-  connect defaultConnectInfo { connectUser = username
+  database <- getEnvDefault "ARKSINK_DB_NAME" "arksink"
+
+  connect defaultConnectInfo { connectHost = host
+                             , connectUser = username
                              , connectPassword = password
                              , connectDatabase = database }
 
